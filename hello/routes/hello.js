37c1586e3bbc58a9bbd4e8b045aed52525757f4e
Samuel Fleet <sfleet@ucsd.edu>
2021-01-19 22:47:22 -0800

    Node.js done

--- hello/routes/hello.js
+++ hello/routes/hello.js
@@ -3,8 +3,20 @@
  * GET home page.
  */
 
-exports.view = function(req, res){
+/*exports.view = function(req, res){
   res.render('index', {
-  	'name': '',
+  	'name': 'Samuel Fleet',
   });
+};
+*/
+
+exports.view = function(req, res) {
+	var nameToShow = req.params.userName;
+	if (nameToShow === undefined) {
+		nameToShow = 'World';
+	}
+	console.log("Name is" + nameToShow);
+	res.render('index', {
+		'name': nameToShow,
+	});
 };
